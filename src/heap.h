6df80fd271e080104beb3aac47ef2caf87edc8b5
autopp <autopp.inc@gmail.com>
2017-01-15 22:22:31 +0900

    add lisplay_malloc_for

--- src/heap.h
+++ src/heap.h
@@ -12,6 +12,8 @@ void lisplay_mark_val(lisplay_cxt_t cxt, lisplay_val_t val);
  * @return      address of allocated memory
  */
 void *lisplay_malloc(lisplay_cxt_t cxt, size_t size);
+#define lisplay_malloc_for(cxt, type) lisplay_malloc((cxt), sizeof(type))
+
 const char *lisplay_strdup(lisplay_cxt_t cxt, lisplay_cstr_t str);
 
 lisplay_root_chunk_t lisplay_create_root(lisplay_cxt_t cxt);
