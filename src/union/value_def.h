61e68eeace7d11e153cc7bc3bf41becef441248e
autopp <autopp.inc@gmail.com>
2017-01-15 15:32:05 +0900

    automatically add comment generated to value_def.h

--- src/union/value_def.h
+++ src/union/value_def.h
@@ -1,7 +1,10 @@
 #ifndef LISPLAY_VALUE_DEF_H
 #define LISPLAY_VALUE_DEF_H
 
-union lisplay_val_t;
+union lisplay_val_t {
+  struct { struct lisplay_obj_header_t header; } dummy;
+};
+
 typedef union lisplay_val_t *lisplay_val_t;
 
 struct lisplay_obj_t;
